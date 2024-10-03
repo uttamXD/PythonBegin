4678a2f5323caf8b933b4f31d0194a3bb8e493b4
Uttam Chamling Rai <uttam.rai@agile.com.np>
2024-10-03 18:01:20 +0545

    String

---
+++ String.py
@@ -0,0 +1,13 @@
+name = "Uttam\n"
+name1 = "Hello! I am Associate ERP developer in Agile Solution."
+
+
+print("My name is", name)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+
+for character in name1:
+    print(character)
